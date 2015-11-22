414dd91be8f8a5682a3a3cc336f38f43274cf35e
admin <masoomulhaqs@gmail.com>
2015-11-22 12:11:35 +0530

    Scripts added, HTML changed

---
+++ js/mk-3Dcarousel.js
@@ -0,0 +1,10 @@
+$.fn.mkCarousel = function(settings){
+	var $target = $(this.selector+'>img'), leftMargin;
+	console.log($(this));
+	var defaults = $.extend({
+		
+	});
+	this.slideIt = function(){
+		console.log('Hurray!');
+	}
+}
