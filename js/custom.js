cd7c917ee672eaaa1751e45aad05fc6f146e4196
admin <masoomulhaqs@gmail.com>
2015-11-22 12:41:47 +0530

    Updates #002

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+$(function(){
+	$('#mkCarousel').mkCarousel({
+		responsive: true,
+		offset: 130,
+		mobileOffset: 60
+	});
+});
--- js/mk-3Dcarousel.js
+++ js/mk-3Dcarousel.js
@@ -1,10 +1,15 @@
-$.fn.mkCarousel = function(settings){
-	var $target = $(this.selector+'>img'), leftMargin;
-	console.log($(this));
-	var defaults = $.extend({
-		
-	});
-	this.slideIt = function(){
-		console.log('Hurray!');
+(function ( $ ) {
+ 
+    
+	$.fn.mkCarousel = function(settings){
+		var $target = $(this.selector+'>img'), leftMargin;
+		var defaults = $.extend({
+			offset: 100,
+			mobileOffset: 40
+		}, settings);
+		this.slideIt = function(){
+		}
 	}
-}
+
+ 
+}( jQuery ));
